fda8ef6d13de3b8729653a534d5f458b9337cb16
kolya <kolya.vladimirsky@gmail.com>
2022-04-26 01:55:18 +0300

    texture template

--- src/Texture.h
+++ src/Texture.h
@@ -1,19 +1,53 @@
 #pragma once
 #include <string>
+#include "vulkan/vulkan.h"
 #include "stb_image.h"
+#include "VulkanHelper.h"
 
 namespace omp{
+/**
+ * @brief Used to load images from PC, and upload directly to GPU
+ */
 class Texture
 {
     std::string m_ContentPath;
     stbi_uc* m_Pixels;
     int m_Size;
+    int m_Width, m_Height;
     uint32_t m_MipLevels;
 
-public:
-    Texture() = default;
-    explicit Texture(const std::string& path);
+    bool m_LoadedToCPU;
+    bool m_LoadedToGPU;
 
-    void LoadTexture(const std::string& path);
+    // Vulkan //
+    // ====== //
+    VkDevice m_LogicalDevice;
+    VkPhysicalDevice m_PhysDevice;
+
+    VkImage m_TextureImage;
+    VkDeviceMemory m_TextureImageMemory;
+    VkImageView m_TextureImageView;
+    VkSampler m_TextureSampler;
+
+    std::weak_ptr<VulkanHelper> m_VkHelper;
+
+public:
+    Texture(VkDevice device, VkPhysicalDevice physDevice, const std::shared_ptr<VulkanHelper> &helper);
+
+    void FullLoad(const std::string& path);
+    void LazyLoad(const std::string& path);
+
+    void Destroy();
+    uint64_t GetTextureId() const;
+
+protected:
+    // Subroutines //
+    // =========== //
+    void LoadTextureToCPU(const std::string& path);
+    void LoadToGPU();
+
+    void createSampler();
+    void createImage();
+    void createImageView();
 };
 } // omp
