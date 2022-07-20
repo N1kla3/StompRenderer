ca8c809ecad28209480d9b794dac41f313f5f812
kolya <kolya.vladimirsky@gmail.com>
2022-07-21 00:38:25 +0300

    GRAPHICS PIPE LIN E

--- src/GraphicsPipeline.cpp
+++ src/GraphicsPipeline.cpp
@@ -1,1 +1,63 @@
 #include "GraphicsPipeline.h"
+
+omp::GraphicsPipeline::~GraphicsPipeline()
+{
+
+}
+
+void omp::GraphicsPipeline::StartCreation()
+{
+    m_IsCreated = false;
+
+}
+
+void omp::GraphicsPipeline::StartDefaultCreation()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateVertexInfo()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateInputAssembly()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateViewport()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateRasterizer()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateMultisamplingInfo()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateColorBlending()
+{
+
+}
+
+void omp::GraphicsPipeline::CreatePipelineLayout()
+{
+
+}
+
+void omp::GraphicsPipeline::CreateShaders(const std::shared_ptr<struct Shader> &shader)
+{
+
+}
+
+void omp::GraphicsPipeline::ConfirmCreation()
+{
+
+    m_IsCreated = true;
+}
--- src/GraphicsPipeline.h
+++ src/GraphicsPipeline.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "vulkan/vulkan.h"
+#include <memory>
 
 
 namespace omp{
@@ -6,11 +8,39 @@ class GraphicsPipeline
 {
 public:
     GraphicsPipeline() = default;
+    ~GraphicsPipeline();
 
     void StartCreation();
+    void StartDefaultCreation();
+    void CreateVertexInfo();
+    void CreateInputAssembly();
+    void CreateViewport();
+    void CreateRasterizer();
+    void CreateMultisamplingInfo();
+    void CreateColorBlending();
+    void CreatePipelineLayout();
+    void CreateShaders(const std::shared_ptr<class Shader>& shader);
     void ConfirmCreation();
 
 private:
-    bool m_IsCreated;
+    bool m_IsCreated = false;
+
+    // PIPELINE //
+    // ======== //
+    VkPipelineVertexInputStateCreateInfo m_VertexInputInfo{};
+    VkPipelineInputAssemblyStateCreateInfo m_InputAssembly{};
+    VkViewport m_Viewport{};
+    VkRect2D m_Scissor{};
+    VkPipelineViewportStateCreateInfo m_ViewportState{};
+    VkPipelineRasterizationStateCreateInfo m_Rasterizer{};
+    VkPipelineMultisampleStateCreateInfo m_Multisampling{};
+    VkPipelineColorBlendStateCreateInfo m_ColorBlending{};
+    VkPushConstantRange m_ConstantRange{};
+    VkPipelineDepthStencilStateCreateInfo m_DepthStencil{};
+
+    VkPipelineLayoutCreateInfo m_PipelineLayoutInfo{};
+
+    VkPipelineLayout m_PipelineLayout;
+    VkPipeline m_GraphicsPipeline;
 };
 }
