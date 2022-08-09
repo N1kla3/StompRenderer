d32fc7441e75aa92d2f3cfe8209d89f574534e25
kolya <kolya.vladimirsky@gmail.com>
2022-08-09 23:54:06 +0300

    update logs

--- src/Logs.h
+++ src/Logs.h
@@ -12,3 +12,7 @@ namespace omp{
 #define INFO(Category, ...) spdlog::get(#Category)->info(__VA_ARGS__)
 #define WARN(Category, ...) spdlog::get(#Category)->warn(__VA_ARGS__)
 #define ERROR(Category, ...) spdlog::get(#Category)->error(__VA_ARGS__)
+
+#define VINFO(Category, ...) spdlog::get(#Category)->info(__FUNCTION__, __VA_ARGS__)
+#define VWARN(Category, ...) spdlog::get(#Category)->warn(__FUNCTION__, __VA_ARGS__)
+#define VERROR(Category, ...) spdlog::get(#Category)->error(__FUNCTION__, __VA_ARGS__)
